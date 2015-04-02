9cce69f9cb9d95664fcb6f58cbbd50a2e74795e8
sambws <saammbb@gmail.com>
2015-04-01 22:20:13 -0700

    fixed untracked files

--- compile.bat
+++
@@ -1,9 +0,0 @@
-cd C:\love-android-sdl2\assets
-del game.zip
-del game.love
-CScript C:\NDK\zip.vbs C:\love\game C:\love-android-sdl2\assets\game.zip
-ren game.zip game.love
-cd ..
-ant debug
-cd C:\Users\sam\AppData\Local\Android\android-sdk\platform-tools
-adb push C:\love-android-sdl2\bin\love_android_sdl2-debug.apk /sdcard/game.apk
--- push.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\sam\AppData\Local\Android\android-sdk\platform-tools
-adb push C:\love-android-sdl2\bin\love_android_sdl2-debug.apk /sdcard/game.apk
